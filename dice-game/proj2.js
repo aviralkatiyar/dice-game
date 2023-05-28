3dd931e7582f6dbf6ab66528159b2b3f70711eac
aviralkatiyar <118202101+aviralkatiyar@users.noreply.github.com>
2023-05-28 16:22:31 +0530

    Add files via upload

---
+++ dice-game/proj2.js
@@ -0,0 +1,29 @@
+var rndInt = Math.floor(Math.random() * 6) + 1;
+
+var randomdiceimage = "dice" + rndInt + ".png";
+
+var randomimagesource= "images/" + randomdiceimage;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimagesource);
+
+var rndInt2 = Math.floor(Math.random() * 6) + 1;
+
+
+var randomimagesource2= "images/dice" + rndInt2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
+if(rndInt>rndInt2){
+
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}
+else if(rndInt>rndInt2){
+
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}
+else{
+
+    document.querySelector("h1").innerHTML="Draw!";
+}
